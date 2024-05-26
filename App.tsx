cd91c2b49ef3a300e781fb1dfd32f9c7b197320b
By_juun <neostgeart@gmail.com>
2024-05-26 16:37:18 +0900

    feat: Home Header

--- App.tsx
+++ App.tsx
@@ -1,118 +1,71 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import Home from './src/screens/Home';
+import Search from './src/screens/Search';
+import Activity from './src/screens/Activity';
+import Profile from './src/screens/Profile';
+import Status from './src/screens/Status';
+import FriendProfile from './src/screens/FriendProfile';
+import EditProfile from './src/screens/EditProfile';
+import Ionic from 'react-native-vector-icons/Ionicons';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const TabBarIcon = ({focused, size, color, route}: any) => {
+  let iconName = '';
+  if (route.name === 'Home') {
+    iconName = focused ? 'home-sharp' : 'home-outline';
+  } else if (route.name === 'Search') {
+    iconName = focused ? 'search' : 'search-outline';
+  } else if (route.name === 'Reels') {
+    iconName = focused
+      ? 'caret-forward-circle'
+      : 'caret-forward-circle-outline';
+  } else if (route.name === 'Activity') {
+    iconName = focused ? 'heart' : 'heart-outline';
+  } else if (route.name === 'Profile') {
+    iconName = focused ? 'person' : 'person-outline';
+  }
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  return <Ionic name={iconName} size={size} color={color} />;
+};
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const BottomTabScreen = () => {
+  const Tab = createBottomTabNavigator();
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarHideOnKeyboard: true,
+        tabBarShowLabel: false,
+        headerShown: false,
+        tabBarStyle: {
+          height: 70,
+        },
+        tabBarIcon: props => TabBarIcon({...props, route}),
+      })}>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Search" component={Search} />
+      <Tab.Screen name="Activity" component={Activity} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+const App = () => {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Bottom" component={BottomTabScreen} />
+        <Stack.Screen name="Status" component={Status} />
+        <Stack.Screen name="FriendProfile" component={FriendProfile} />
+        <Stack.Screen name="EditProfile" component={EditProfile} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default App;
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,14 +1,55 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import React from 'react';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={styles.background}>
+      <StatusBar backgroundColor="white" barStyle="dark-content" />
+      <View style={styles.header}>
+        <View>
+          <Text style={styles.title}>Instagram</Text>
+        </View>
+        <View style={styles.right}>
+          <FontAwesome name="plus-square-o" style={styles.plusIcon} />
+          <Feather name="navigation" style={styles.featherIcon} />
+        </View>
+      </View>
+      {/* <ScrollView>
+        <Stories />
+        <Post />
+      </ScrollView> */}
+    </SafeAreaView>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  background: {backgroundColor: 'white'},
+  header: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    paddingHorizontal: 15,
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: '500',
+  },
+  right: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  plusIcon: {fontSize: 24, paddingHorizontal: 15},
+  featherIcon: {fontSize: 24},
+});
